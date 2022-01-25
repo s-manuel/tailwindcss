313242736caa6858633af8d9124ba5e78b46dca2
s-manuel <guangasitsm@gmail.com>
2022-01-25 08:03:01 -0500

    Hola mundo

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+  
+    <div class="container mx-auto mt-5 mx-5">
+        <h1 class="text-3xl font-bold underline py-5 sm:text-stone-300">
+            Hello world!
+        </h1>
+        <div class="sm:bg-orange-300 md:bg-orange-100 p-5">
+            <button class='bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700'>
+                Botón con Tail
+            </button>
+        </div>
+        <div class="m-4">
+            <a href="https://bluuweb.github.io/tailwindcss/01-fundamentos/#container">Curso</a>
+            <a href="https://tailwindcss.com/docs/background-color">Documentación</a>
+        </div>
+    </div>
+</body>
+</html>
